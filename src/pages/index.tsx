4c82da8ea92c3eea4825be4eb8fb9fcab4a2e256
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2021-03-20 18:33:58 +0900

    exc format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,10 @@ import IndexLayout from '../layouts'
 
 const IndexPage = () => (
   <IndexLayout>
-        <h1>Hi people</h1>
-        <p>Welcome to your new Gatsby site.</p>
-        <p>Now go build something great.</p>
-        <Link to="/page-2/">Go to page 2</Link>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <Link to="/page-2/">Go to page 2</Link>
   </IndexLayout>
 )
 
